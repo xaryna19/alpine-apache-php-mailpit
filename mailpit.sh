fa851081b1b65ce4a1546c9e78d1df82050bf74d
xaryna19 <xaryna19@gmail.com>
2024-09-29 08:53:43 +0800

    Update mailpit.sh

--- mailpit.sh
+++ mailpit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $$ > /run/mailpit.pid  # Store the PID of this process
-/usr/local/bin/mailpit -d /var/lib/mailpit/mailpit.db -m 1000 --smtp-auth-allow-insecure --quiet
+/usr/local/bin/mailpit -d /var/lib/mailpit/mailpit.db -m 1000 --smtp-auth-allow-insecure
