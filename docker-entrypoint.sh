579962701c4f7ac0d57d6da4bc5e2845dacffdf5
xaryna19 <xaryna19@gmail.com>
2024-09-29 11:32:18 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -59,8 +59,8 @@ sed -i "s#^;date.timezone =\$#date.timezone = \"${TZ}\"#" /etc/php83/php.ini
 sed -i 's/^ServerTokens Full/ServerTokens ${HOST_ENV}/' /etc/apache2/httpd.conf
 sed -i 's/^ServerSignature Off/ServerSignature On/' /etc/apache2/httpd.conf
 
-echo 'mailpit is ' ${MAILPIT}
-if [${MAILPIT} = "enabled" ]
+echo 'mailpit is' ${MAILPIT}
+if [${MAILPIT} == "enabled" ]
 then
   service mailpit start
 else
